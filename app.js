8705c34726fb88abb3e1cf9ac9e0614d5abe3446
gavinoconnor <gedmund@gmail.com>
2019-11-19 17:54:41 -0500

    add routes

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+let campgrounds = [
+{name: "Salmon Creek", image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
+{name: "Granite Hill", image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
+{name: "Balder's Pass", image: "https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"}
+];
 
 app.get("/", function(req, res) {
   res.render("landing");
-})
+  })
 
 app.get("/campgrounds", function(req, res) {
-  let campgrounds = [
-    {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
-    {name: "Granite Hill", image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
-    {name: "Balder's Pass", image: "https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"}
-  ];
   res.render("campgrounds", {campgrounds: campgrounds});
-})
+});
+
+app.post("/campgrounds", function(req, res){
+  let name = req.body.name;
+  let image = req.body.image;
+  let newCampground = {name: name, image: image}
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("new")
+});
 
 app.listen(3000, function() {
   console.log("YelpCamp Server Has Started.")
